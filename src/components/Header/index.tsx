118e0e40c38eaace124a1200fa7a6bc427b01ee4
Felipe Duarte <felipesilvapac@gmail.com>
2021-11-03 17:58:41 -0300

    Content Header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
 import logo from '../../assets/logo.svg';
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 export function Header() {
   return (
     <Container>
+      <Content>
       <img src={logo} alt="dt money" />
       <button type="button">Nova Transação</button>
+      </Content>
     </Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,3 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.header`
   background: var(--blue);
 `;
+
+export const Content = styled.div`
+  max-width: 11200px;
+  margin: 0 auto;
+`;
