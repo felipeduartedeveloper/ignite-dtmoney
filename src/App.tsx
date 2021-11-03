106cc49cb1436e5c1cab51d0d328ae800caa446b
Felipe Duarte <felipesilvapac@gmail.com>
2021-11-03 17:26:21 -0300

    global css styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./assets/styles/global";
+
 export function App() {
   return (
     <div className="App">
      <h1>helloo</h1>
+     <GlobalStyle/>
     </div>
   );
 }
