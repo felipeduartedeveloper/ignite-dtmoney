29f961ad1f90045c955d235a3c6577bd69f9de9b
felipeduartedeveloper <felipesilvapac@gmail.com>
2021-11-04 00:18:31 -0300

    Summary styles ok

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,34 @@
+import entradasImg from '../../assets/entradas.svg';
+import saidasImg from '../../assets/saidas.svg';
+import totalImg from '../../assets/total.svg';
+import { Container } from "./styles";
+
 export function Summary() {
   return (
-    <h1>Summary</h1>
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={entradasImg} alt=""></img>
+        </header>
+        <strong>R$10000</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={saidasImg} alt=""></img>
+        </header>
+        <strong>- R$10000</strong>
+      </div>
+
+      <div className='highlight-background'>
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt=""></img>
+        </header>
+        <strong>R$10000</strong>
+      </div>
+    </Container>
   )
 }
