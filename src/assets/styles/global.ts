d68035df61b493e56dabe57415c4ac1c5bc20f6a
Felipe Duarte <felipesilvapac@gmail.com>
2021-11-04 16:41:08 -0300

    modal close button ok

--- src/assets/styles/global.ts
+++ src/assets/styles/global.ts
@@ -82,6 +82,10 @@ export const GlobalStyle = createGlobalStyle`
     top: 1.5rem;
     border: 0;
     background: transparent;
-  }
+    transition: filter 0.2s;
 
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
 `;
