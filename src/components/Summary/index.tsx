920abf3510a53eed1b07a1226f9327ebbf36d4d0
Felipe Duarte <felipesilvapac@gmail.com>
2021-11-05 11:43:20 -0300

    use Context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,11 +1,16 @@
+import React, { useContext } from 'react';
 import entradasImg from '../../assets/entradas.svg';
 import saidasImg from '../../assets/saidas.svg';
 import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
 import { Container } from "./styles";
 
 export function Summary() {
+  const data = useContext(TransactionsContext);
+
   return (
     <Container>
+ 
       <div>
         <header>
           <p>Entradas</p>
